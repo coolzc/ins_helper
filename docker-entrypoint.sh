e3873b1323173686553f7f3753aecfc5b1161b79
coolzc <coolzc@gmail.com>
2017-07-26 17:08:49 +0800

    modify build.sh and add adsense

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sh build-dashboard.sh
 pm2 start /usr/src/app/server/server.js --name inshelper -i 0 -o ./inshelper/output.log -e ./inshelper/error.log --no-daemon
--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-cd .. && docker build -t registry.us-west-1.aliyuncs.com/inshelper/inshelper:$1 . && docker push registry.us-west-1.aliyuncs.com/inshelper/inshelper:$1
-cd ./docker
+cd .. && sh build-app.sh && docker build -t registry.us-west-1.aliyuncs.com/inshelper/inshelper:$1 . && docker push registry.us-west-1.aliyuncs.com/inshelper/inshelper:$1
