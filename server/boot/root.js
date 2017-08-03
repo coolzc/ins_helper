2618b804c1310477af9c62e39395b20865777391
coolzc <coolzc@gmail.com>
2017-08-03 16:56:47 +0800

    add robots file

--- server/boot/root.js
+++ server/boot/root.js
@@ -6,15 +6,17 @@ const loopback = require('loopback')
 module.exports = function (server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router()
-  router.get('/', (req, res) => {
-    res.redirect('index.html')
+  //router.get('/', (req, res) => {
+  //  res.sendFile(path.resolve(__dirname, '../../client/app', 'index.html'))
+  //})
+
+  server.get('/robots.txt', function (req, res) {
+    res.type('text/plain')
+    res.send("User-agent: *\nDisallow:")
   })
-  router.get('/ins', (req, res) => {
-    res.end()
-    InsSaveHelper.savePicture('https://www.instagram.com/p/BReNCbLD-w6')
-  })
+
   server.use(router)
-  const dist = path.resolve(__dirname, '../../client/app')
+  const dist = path.resolve(__dirname, '../../client/app/')
   server.use(loopback.static(dist))
 }
 
